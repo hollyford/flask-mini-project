bf84f32ccc05b865f6921ca64b237965b81651c5
hollyford <holly_ford@hotmail.com>
2021-01-16 13:30:20 +0000

    add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -2,6 +2,14 @@
     $('.sidenav').sidenav({edge: "right"});
     $('.collapsible').collapsible();
     $('.tooltipped').tooltip();
+    $('.datepicker').datepicker({
+        format: "dd mmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
   });
 
   
